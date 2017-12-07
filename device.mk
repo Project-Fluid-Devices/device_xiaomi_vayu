af42b5f28f8dfff938bf8beff2fd2af5c278e6e8
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    vayu: Build dummy android.hidl.[base,manager]@1.0
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c

--- device.mk
+++ device.mk
@@ -46,6 +46,11 @@ PRODUCT_PACKAGES += \
     libqcomfm_jni \
     qcom.fmradio
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
