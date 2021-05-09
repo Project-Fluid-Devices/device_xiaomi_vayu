0de51f860d76fc58e259fbef63a1b4dbae277e8b
Foxtrot47 <jjneutron@outlook.com>
2021-05-09 23:08:28 +0530

    vayu: props:  Reduce Volume Steps
    overriding it in init also

--- init/init_vayu.cpp
+++ init/init_vayu.cpp
@@ -104,4 +104,6 @@ void vendor_load_properties() {
     if (mod_device != "") {
         property_override("ro.product.mod_device", mod_device.c_str());
     }
+    property_override("ro.config.media_vol_steps", "7");
+    property_override("ro.config.vc_call_vol_steps", "5");
 }
--- product_prop.mk
+++ product_prop.mk
@@ -15,8 +15,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.media_vol_default=10 \
-	ro.config.media_vol_steps=150 \
-	ro.config.vc_call_vol_steps=11
+	ro.config.media_vol_steps=7 \
+	ro.config.vc_call_vol_steps=5
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
