fc70c91923448cb0cc6cb39f19708b68733209b9
Foxtrot47 <jjneutron@outlook.com>
2021-10-28 13:33:11 +0530

    vayu: Use Aosp Clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,8 +23,7 @@ TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_vayu
 
 # Kernel
 TARGET_KERNEL_CONFIG := vayu_defconfig
-TARGET_KERNEL_CLANG_VERSION := proton
-TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
+TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld
 
 # Inherit from the proprietary version
 include vendor/xiaomi/vayu/BoardConfigVendor.mk
